65ba4681661f944251680643ea6dc4466c0a94c7
Granton Mchana <gmchana254@gmail.com>
2017-09-20 14:35:24 +0300

    complitions

--- app/routes/index.js
+++ app/routes/index.js
@@ -25,16 +25,21 @@ var rentals = [{
 
 export default Ember.Route.extend({
   model() {
-  return Ember.RSVP.hash({
-    rentals: this.store.findAll('rental'),
-    reviews: this.store.findAll('review')
-  });
-},
-   actions: {
+    return Ember.RSVP.hash({
+      rentals: this.store.findAll('rental'),
+      reviews: this.store.findAll('review')
+    });
+  },
+  actions: {
     saveRental3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
       this.transitionTo('index');
+    },
+    saveReview(params) {
+      var newReview = this.store.createRecord('review', params);
+      newReview.save();
+      this.transitionTo('index');
     }
   }
-})
+});
