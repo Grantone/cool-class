dd650118f826d7015230b3891d33fd054073a9cb
Granton Mchana <gmchana254@gmail.com>
2017-09-21 14:58:29 +0300

    additions

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -4,8 +4,13 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('rental', params.rental_id);
   },
-  destroyReview(review) {
-      review.destroyRecord();
-      this.transitionTo('index');
-    }
+  destroyRental(rental) {
+     var review_deletions = rental.get('reviews').map(function(review) {
+       return review.destroyRecord();
+     });
+     Ember.RSVP.all(review_deletions).then(function() {
+       return rental.destroyRecord();
+     });
+     this.transitionTo('index');
+   },
 });
